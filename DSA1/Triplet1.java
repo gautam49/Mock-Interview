10e4bae723af737a304eb4d227181cb7c0bae757
gautam <gautampal560@gmail.com>
2022-08-04 16:38:41 +0530

    4 august 2022

--- DSA1/Triplet1.java
+++ DSA1/Triplet1.java
@@ -1,7 +1,34 @@
-package com.MockInterview;
+package MockInterview.DSA1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.io.*;
 
 public class Triplet1 {
-    public static void main(String[] args) {
+    public static boolean triplet(int[] arr,int sum)
+    {
+        HashSet<Integer> s=new HashSet<>();
+        for(int i=0;i<arr.length-2;i++)
+        {
+          int temp=sum-arr[i];
+          for(int j=i+1;j< arr.length;j++)
+          {
+              if(s.contains(temp-arr[j]))
+              {
+                  System.out.println(arr[i]+" "+ (temp-arr[j])+" "+arr[j]);
+                  return true;
+              }
+              s.add(arr[j]);
+          }
 
+        }
+        return false;
     }
+    public static void main(String[] args) {
+        int[] arr={3,4,5,6,7,8,9};
+        int sum=20;
+         triplet(arr,sum);
+    }
+
+    
 }
